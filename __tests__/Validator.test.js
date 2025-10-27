a7eb56a9361e23528ed5d1a7053e583ab7eff953
suucong <suucong2@swu.ac.kr>
2025-10-27 17:19:45 +0900

    refactor(test): Validator 테스트 구조화 및 명세 정확성 개선
    - Validator 테스트의 가독성을 위해 테스트마다 describe를 추가
    - 정상 입력 케이스를 추가하여 테스트 범위 확장
    - 허용 문자 예외 테스트에서 유효한 것으로 판명된 케이스 제외

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -2,49 +2,61 @@ import Validator from "../src/Validator.js";
 import { ERROR } from "../src/constants/Messages.js";
 
 describe("입력 유효성 검사 (Validator)", () => {
-  test("정상적인 입력은 trim 후 이름 배열을 반환해야 한다.", () => {
-    const input = " pobi, woni, jun";
-    const result = Validator.validateAndParseNames(input);
-    expect(result).toEqual(["pobi", "woni", "jun"]);
+  describe("자동차 이름 정상 파싱", () => {
+    test.each([
+      [" pobi, woni, jun ", ["pobi", "woni", "jun"]],
+      ["su ji, pobi, 123", ["su ji", "pobi", "123"]],
+      ["short", ["short"]],
+    ])(
+      "정상적인 입력은 trim 후 이름 배열을 반환해야 한다: %s",
+      (input, expected) => {
+        const result = Validator.validateAndParseNames(input);
+        expect(result).toEqual(expected);
+      }
+    );
   });
 
-  test.each(["pobi,woni,toolong", "안녕나는루피", "안녕나는포비,자동차"])(
-    "이름이 5자 초과인 경우, 에러를 발생시킨다: %s",
-    (input) => {
-      expect(() => Validator.validateAndParseNames(input)).toThrow(
-        ERROR.NAME_LENGTH
-      );
-    }
-  );
+  describe("자동차 이름 길이가 5자 초과하는 경우 예외 발생", () => {
+    test.each(["pobi,woni,toolong", "안녕나는루피", "안녕나는포비,자동차"])(
+      "이름이 5자 초과인 경우, 에러를 발생시킨다: %s",
+      (input) => {
+        expect(() => Validator.validateAndParseNames(input)).toThrow(
+          ERROR.NAME_LENGTH
+        );
+      }
+    );
+  });
 
-  test.each(["pobi, woni, pobi", "수진,수진", " pobi,  pobi "])(
-    "이름이 중복되는 경우, 에러를 발생시킨다: %s",
-    (input) => {
-      expect(() => Validator.validateAndParseNames(input)).toThrow(
-        ERROR.NAME_DUPLICATE
-      );
-    }
-  );
+  describe("자동차 이름이 중복되는 경우 예외 발생", () => {
+    test.each(["pobi, woni, pobi", "수진,수진", " pobi,  pobi "])(
+      "이름이 중복되는 경우, 에러를 발생시킨다: %s",
+      (input) => {
+        expect(() => Validator.validateAndParseNames(input)).toThrow(
+          ERROR.NAME_DUPLICATE
+        );
+      }
+    );
+  });
 
-  test.each(["pobi,,jun", ",pobi,woni", "pobi,수진, ", ",,"])(
-    "이름이 빈 문자열인 경우, 에러를 발생시킨다: %s",
-    (input) => {
-      expect(() => Validator.validateAndParseNames(input)).toThrow(
-        ERROR.NAME_EMPTY
-      );
-    }
-  );
+  describe("자동차 이름이 빈 문자열인 경우 예외 발생", () => {
+    test.each(["pobi,,jun", ",pobi,woni", "pobi,수진, ", ",,"])(
+      "이름이 빈 문자열인 경우, 에러를 발생시킨다: %s",
+      (input) => {
+        expect(() => Validator.validateAndParseNames(input)).toThrow(
+          ERROR.NAME_EMPTY
+        );
+      }
+    );
+  });
 
-  test.each([
-    "pobi, woni!",
-    "pobi,woni@",
-    "pobi,수진#",
-    "a lice, 수진",
-    "pobi, w123-",
-    "pobi, woni\n",
-  ])("허용되지 않은 문자가 포함된 경우, 에러를 발생시킨다: %s", (input) => {
-    expect(() => Validator.validateAndParseNames(input)).toThrow(
-      ERROR.NAME_INVALID_CHAR
+  describe("자동차 이름에 허용되지 않은 문자가 포함된 경우 예외 발생", () => {
+    test.each(["pobi, woni!", "pobi,woni@", "pobi,수진#", "pobi, w123-"])(
+      "허용되지 않은 문자가 포함된 경우, 에러를 발생시킨다: %s",
+      (input) => {
+        expect(() => Validator.validateAndParseNames(input)).toThrow(
+          ERROR.NAME_INVALID_CHAR
+        );
+      }
     );
   });
 });
