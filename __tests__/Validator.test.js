cb9c7669e49ccc9639500d24117fb528ef336f9c
suucong <suucong2@swu.ac.kr>
2025-10-27 15:12:04 +0900

    test(Validator): 자동차 이름 길이(5자 이하) 검증 추가

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -1,4 +1,5 @@
 import Validator from "../src/Validator.js";
+import { ERROR } from "../src/constants/Messages.js";
 
 describe("입력 유효성 검사 (Validator)", () => {
   test("정상적인 입력은 trim 후 이름 배열을 반환해야 한다.", () => {
@@ -6,4 +7,13 @@ describe("입력 유효성 검사 (Validator)", () => {
     const result = Validator.validateAndParseNames(input);
     expect(result).toEqual(["pobi", "woni", "jun"]);
   });
+
+  test.each(["pobi,woni,toolong", "안녕나는루피", "안녕나는포비,자동차"])(
+    "이름이 5자 초과인 경우, 에러를 발생시킨다: %s",
+    (input) => {
+      expect(() => Validator.validateCarNames(input)).toThrow(
+        ERROR.NAME_LENGTH
+      );
+    }
+  );
 });
