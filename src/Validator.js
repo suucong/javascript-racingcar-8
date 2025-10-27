e285fa38a22d9354f9c9a3b3103402e6b17b9ac7
suucong <suucong2@swu.ac.kr>
2025-10-27 15:26:42 +0900

    feat(Validator): 자동차 이름이 5자 초과일 경우 예외 처리 구현

--- src/Validator.js
+++ src/Validator.js
@@ -1,6 +1,14 @@
+import { ERROR } from "./constants/Messages.js";
+
 class Validator {
   static validateAndParseNames(inputString) {
     const names = inputString.split(",").map((name) => name.trim());
+
+    const isOverFive = names.some((name) => name.length > 5);
+    if (isOverFive) {
+      throw new Error(ERROR.NAME_LENGTH);
+    }
+
     return names;
   }
 
