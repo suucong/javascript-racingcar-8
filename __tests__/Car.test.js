b4562e9166c20e350801986098621d110d45a4db
suucong <suucong2@swu.ac.kr>
2025-10-27 20:13:23 +0900

    test(Car): 난수 주입 방식으로 변경하여 move 로직 테스트 단순화

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,55 +1,44 @@
-import Car from "../src/Car.js";
-import { Random } from "@woowacourse/mission-utils";
+import Car from "../Car.js";
 
 describe("Car 클래스 기능 테스트", () => {
-  beforeEach(() => {
-    jest.spyOn(Random, "pickNumberInRange");
-  });
-
-  afterEach(() => {
-    jest.restoreAllMocks();
-  });
-
   test("Car 객체는 이름과 초기 위치 0을 가진다.", () => {
     const car = new Car("pobi");
-    expect(car.name).toBe("pobi");
-    expect(car.position).toBe(0);
+    expect(car.getName()).toBe("pobi");
+    expect(car.getPosition()).toBe(0);
   });
 
-  describe("move() 메서드 동작 검증", () => {
+  describe("move(randomNumber) 메서드 동작 검증", () => {
     let car;
 
     beforeEach(() => {
       car = new Car("test");
     });
 
-    test("난수 값이 3 이하일 경우 (멈춤), position은 변하지 않는다.", () => {
-      Random.pickNumberInRange.mockReturnValue(3);
-      car.move();
-      car.move();
-      expect(car.position).toBe(0);
-    });
+    test.each([0, 1, 2, 3])(
+      "난수 값이 3 이하일 경우 (멈춤), position은 변하지 않는다: %s",
+      (value) => {
+        car.move(value);
+        car.move(value);
+        expect(car.getPosition()).toBe(0);
+      }
+    );
 
-    test("난수 값이 4 이상일 경우 (전진), position이 1 증가한다.", () => {
-      Random.pickNumberInRange.mockReturnValue(4);
-      car.move();
-      car.move();
-      expect(car.position).toBe(2);
-    });
+    test.each([4, 5, 6, 9])(
+      "난수 값이 4 이상일 경우 (전진), position이 1 증가한다: %s",
+      (value) => {
+        car.move(value);
+        car.move(value);
+        expect(car.getPosition()).toBe(2);
+      }
+    );
 
-    test("난수 값에 따라 전진과 멈춤이 혼합되어 동작한다.", () => {
-      Random.pickNumberInRange
-        .mockReturnValueOnce(3)
-        .mockReturnValueOnce(4)
-        .mockReturnValueOnce(3)
-        .mockReturnValueOnce(4);
+    test("주입된 난수 값에 따라 전진과 멈춤이 혼합되어 동작한다.", () => {
+      car.move(3);
+      car.move(4);
+      car.move(3);
+      car.move(4);
 
-      car.move();
-      car.move();
-      car.move();
-      car.move();
-
-      expect(car.position).toBe(2);
+      expect(car.getPosition()).toBe(2);
     });
   });
 });
